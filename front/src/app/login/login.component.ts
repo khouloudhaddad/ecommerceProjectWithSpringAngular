194711d65685e57645f856461b88cf160e2fec54
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-03-20 10:05:42 +0100

    Create login page

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {AuthService} from "../services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm!: FormGroup;
+  hidePassword= true;
 
+  constructor(private fb: FormBuilder,
+              private snackBar: MatSnackBar,
+              private authService: AuthService,
+              private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit(): void {
+    const username = this.loginForm.get('email')!.value;
+    const password = this.loginForm.get('password')!.value;
+
+    this.authService.login(username, password.subscribe(
+        (res: any) => {
+          this.snackBar.open("User successfully logged in.", 'Ok', { duration: 5000 })
+        },
+        (error: any) => {
+          this.snackBar.open("Bad credentials", 'ERROR', { duration: 5000})
+        }
+      )
+    )
+  }
 }
