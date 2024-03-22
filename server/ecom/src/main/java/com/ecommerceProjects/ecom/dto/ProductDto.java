792585519fdb6e95ae2ac4f6501a90fe5403ab66
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-03-22 13:38:55 +0100

    Create category front

--- server/ecom/src/main/java/com/ecommerceProjects/ecom/dto/ProductDto.java
+++ server/ecom/src/main/java/com/ecommerceProjects/ecom/dto/ProductDto.java
@@ -1,34 +1,16 @@
 package com.ecommerceProjects.ecom.dto;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 260517517e59aa450fddc00a958890adf78a607e
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class ProductDto {
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 260517517e59aa450fddc00a958890adf78a607e
     private Long id;
     private String name;
     private Long price;
     private String description;
     private byte[] byteImg;
-<<<<<<< HEAD
     private Long categoryId;
-=======
-    
-    private Long categoryId;
-    
->>>>>>> 260517517e59aa450fddc00a958890adf78a607e
     private MultipartFile img;
 }
-=======
 
->>>>>>> e4efdb51baa5b15b770e8050338f78fcb2dabf16
--- server/ecom/src/main/java/com/ecommerceProjects/ecom/services/admin/product/ProductServiceImpl.java
+++ server/ecom/src/main/java/com/ecommerceProjects/ecom/services/admin/product/ProductServiceImpl.java
@@ -19,21 +19,11 @@ public class ProductServiceImpl implements ProductService{
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
     
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public ProductDto createProduct(ProductDto productDto) throws IOException
-    {
-        Product product = new Product();
-        product.setName(productDto.getName());
-        product.setDescription(productDto.getDescription());
-        product.setPrice(productDto.getPrice());
-=======
     public ProductDto createProduct(ProductDto productDto) throws IOException {
         Product product = new Product();
         product.setName(product.getName());
         product.setDescription(product.getDescription());
         product.setPrice(product.getPrice());
->>>>>>> 260517517e59aa450fddc00a958890adf78a607e
         product.setImg(productDto.getImg().getBytes());
 
         Category category = categoryRepository.findById(productDto.getCategoryId()).orElseThrow();
@@ -43,16 +33,9 @@ public class ProductServiceImpl implements ProductService{
         return productRepository.save(product).getDto();
     }
     
-<<<<<<< HEAD
-    public List<ProductDto> getAllProducts()
-    {
-=======
     public List<ProductDto> getAllProducts(){
->>>>>>> 260517517e59aa450fddc00a958890adf78a607e
         List<Product> products = productRepository.findAll();
         return products.stream().map(Product::getDto).collect(Collectors.toList());
     }
 }
-=======
 
->>>>>>> e4efdb51baa5b15b770e8050338f78fcb2dabf16
